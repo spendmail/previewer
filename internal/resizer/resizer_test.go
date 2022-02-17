8c838559d4562a4d28b2bedee476528c1937b1aa
spendmail <info@spendmail.ru>
2022-02-17 15:03:46 +0700

    added resizer tests

--- internal/resizer/resizer_test.go
+++ internal/resizer/resizer_test.go
@@ -1,90 +1,45 @@
-package app
+package resizer
 
 import (
 	"bytes"
-	"errors"
+	"context"
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	"io"
 	"net/http"
 	"testing"
 
-	internalcache "github.com/spendmail/previewer/internal/cache"
-	internalconfig "github.com/spendmail/previewer/internal/config"
-	internallogger "github.com/spendmail/previewer/internal/logger"
-	internalresizer "github.com/spendmail/previewer/internal/resizer"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	ImageWidth           = 300
-	ImageHeight          = 200
-	ImageURL             = "raw.githubusercontent.com/OtusGolang/final_project/master/examples/image-previewer/gopher_2000x1000.jpg"
-	WrongImageURLPath    = "raw.githubusercontent.com/mistake_in_the_path"
-	WrongDNSURL          = "this-is-non-existent-domain.com/image.jpeg"
-	ContentTypeImageJpeg = "image/jpeg"
+	ImageWidth  = 300
+	ImageHeight = 200
+	Scheme      = "http://"
+	ImageURL    = "raw.githubusercontent.com/OtusGolang/final_project/master/examples/image-previewer/gopher_2000x1000.jpg"
 )
 
 func TestApplication(t *testing.T) {
-	t.Run("succeeding test", func(t *testing.T) {
-		config, err := internalconfig.NewConfig("../../configs/previewer.toml")
+	t.Run("resizing test", func(t *testing.T) {
+		resizer := New()
+
+		request, err := http.NewRequestWithContext(context.Background(), http.MethodGet, Scheme+ImageURL, nil)
 		require.NoError(t, err, "should be without errors")
 
-		logger, err := internallogger.New(config)
+		response, err := http.DefaultClient.Do(request)
+		require.NoError(t, err, "should be without errors")
+		defer response.Body.Close()
+
+		imageBytes, err := io.ReadAll(response.Body)
 		require.NoError(t, err, "should be without errors")
 
-		cache, err := internalcache.New(config, logger)
+		croppedImageBytes, err := resizer.Resize(uint(ImageWidth), uint(ImageHeight), imageBytes)
 		require.NoError(t, err, "should be without errors")
 
-		app, err := New(logger, internalresizer.New(), cache)
-		require.NoError(t, err, "should be without errors")
-
-		headers := map[string][]string{}
-		imageBytes, err := app.ResizeImageByURL(ImageWidth, ImageHeight, ImageURL, headers)
-		require.NoError(t, err, "should be without errors")
-
-		bytesContentType := http.DetectContentType(imageBytes)
-		require.Equal(t, ContentTypeImageJpeg, bytesContentType, fmt.Sprintf("content type should be %s, but %s given", ContentTypeImageJpeg, bytesContentType))
-
-		img, _, err := image.DecodeConfig(bytes.NewReader(imageBytes))
+		img, _, err := image.DecodeConfig(bytes.NewReader(croppedImageBytes))
 		require.NoError(t, err, "should be without errors")
 		require.Equal(t, ImageWidth, img.Width, fmt.Sprintf("image width should be %d, but %d given", ImageWidth, img.Width))
 		require.Equal(t, ImageHeight, img.Height, fmt.Sprintf("image height should be %d, but %d given", ImageHeight, img.Height))
-	})
-
-	t.Run("wrong dns", func(t *testing.T) {
-		config, err := internalconfig.NewConfig("../../configs/previewer.toml")
-		require.NoError(t, err, "should be without errors")
-
-		logger, err := internallogger.New(config)
-		require.NoError(t, err, "should be without errors")
-
-		cache, err := internalcache.New(config, logger)
-		require.NoError(t, err, "should be without errors")
-
-		app, err := New(logger, internalresizer.New(), cache)
-		require.NoError(t, err, "should be without errors")
-
-		headers := map[string][]string{}
-		_, err = app.ResizeImageByURL(ImageWidth, ImageHeight, WrongDNSURL, headers)
-		require.Truef(t, errors.Is(err, ErrServerNotExists), "actual error %q", err)
-	})
-
-	t.Run("file not found", func(t *testing.T) {
-		config, err := internalconfig.NewConfig("../../configs/previewer.toml")
-		require.NoError(t, err, "should be without errors")
-
-		logger, err := internallogger.New(config)
-		require.NoError(t, err, "should be without errors")
-
-		cache, err := internalcache.New(config, logger)
-		require.NoError(t, err, "should be without errors")
-
-		app, err := New(logger, internalresizer.New(), cache)
-		require.NoError(t, err, "should be without errors")
-
-		headers := map[string][]string{}
-		_, err = app.ResizeImageByURL(ImageWidth, ImageHeight, WrongImageURLPath, headers)
-		require.Truef(t, errors.Is(err, ErrFileNotFound), "actual error %q", err)
 	})
 }
